e972bee7518aa920378f17ca963d155a5231ea5e
sophistcxf <sophistcxf@gmail.com>
2016-06-12 21:31:03 +0800

    modify libevent http

--- libevent/src/test_http.cpp
+++ libevent/src/test_http.cpp
@@ -9,10 +9,19 @@
 using namespace std;
 
 #include "event2/http.h"
+#include "event2/http_struct.h"
 #include "event2/event.h"
 #include "event2/buffer.h"
+#include "event2/keyvalq_struct.h"
 
 void http_request_handler(struct evhttp_request *req, void *arg) { 
+  cout << "remote host: " << req->remote_host << endl;
+  cout << "uri: " << req->uri << endl;
+  cout << "host_cache: " << req->host_cache << endl;
+  struct evkeyvalq* input_headers = req->input_headers;
+  for (struct evkeyval* header = input_headers->tqh_first; header; header = header->next.tqe_next) {
+    cout << "key: " << header->key << ", value: " << header->value << endl;
+  }
   struct evbuffer *databuf = evbuffer_new(); 
   evbuffer_add_printf(databuf, "hello world");
   evhttp_send_reply_start(req,200,"OK");
