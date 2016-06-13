d81383aa4d4a0044025559b1a2c12e9f5becb6bc
sophistcxf <sophistcxf@gmail.com>
2016-06-13 17:33:42 +0800

    add rapid json

---
+++ boost/src/test_log.cpp
@@ -0,0 +1,35 @@
+/*************************************************************************
+  > File Name: test_log.cpp
+  > Author: xiangfei.cui
+  > Mail: xiangfei.cui@alibaba-inc.com
+  > Created Time: Mon 13 Jun 2016 02:35:42 PM CST
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+#include <boost/log/common.hpp>
+#include <boost/log/sinks.hpp>
+#include <boost/log/sources/logger.hpp>
+#include <boost/utility/empty_deleter.hpp>
+#include <boost/shared_ptr.hpp>
+#include <iostream>
+
+using namespace boost::log;
+
+int main()
+{
+  typedef sinks::asynchronous_sink<sinks::text_ostream_backend> text_sink;
+  boost::shared_ptr<text_sink> sink = boost::make_shared<text_sink>();
+
+  boost::shared_ptr<std::ostream> stream{&std::clog,
+    boost::empty_deleter{}};
+  sink->locked_backend()->add_stream(stream);
+
+  core::get()->add_sink(sink);
+
+  sources::logger lg;
+
+  BOOST_LOG(lg) << "note";
+  sink->flush();
+}
