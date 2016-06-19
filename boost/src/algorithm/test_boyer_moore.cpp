f55520e017183e7fc1092012a474771cda4d5641
sophistcxf <sophistcxf@github.com>
2016-06-19 05:07:19 -0700

    modify boyer moore

--- boost/src/algorithm/test_boyer_moore.cpp
+++ boost/src/algorithm/test_boyer_moore.cpp
@@ -8,8 +8,11 @@ int main() {
 	std::string corpus("here is a simple example");
 	std::string pattern("example");
 
+  std::string pattern1("hhhohhhoo");
 	boost::algorithm::boyer_moore<std::string::const_iterator>
 		search(pattern.begin(), pattern.end());
+	boost::algorithm::boyer_moore<std::string::const_iterator>
+		search2(pattern1.begin(), pattern1.end());
 
 	if (corpus.end() != search(corpus.begin(), corpus.end())) {
 		std::cout << "pattern found" << std::endl;
