81969c021a1f4ac8c823163edee789de741f4925
sophistcxf <sophistcxf@gmail.com>
2016-07-09 15:59:24 +0800

    modi resource limit

--- linux/test_rlimit.cpp
+++ linux/test_rlimit.cpp
@@ -7,6 +7,7 @@
 
 /*
  * test getrlimit and setrlimit 
+ * what's the difference between rlim_cur and rlim_max?
  */
 
 #include <iostream>
@@ -14,16 +15,28 @@
 using namespace std;
 
 #include <sys/resource.h>
+#include <errno.h>
 
-extern int errno;
+static vector<string> RESOURCES;
+/*
+ * __RLIMIT_NLIMITS is defined in bits/resources.h
+ */
+static void initResources
+{
+  
+}
 
-int main()
+void print_rlimit(int resource)
 {
   struct rlimit fd_limit;
-  if (getrlimit(RLIMIT_NOFILE, &fd_limit) < 0) {
-    fprintf(stderr, "getrlimit(RLIMIT_NOFILE, &fd_limit) error: %d", errno);
+  if (getrlimit(resource, &fd_limit) < 0) {
+    fprintf(stderr, "getrlimit(%d, &fd_limit) error: %d", errno);
     return -1;
   }
   printf("file descriptor, cur limit: %d, max limit: %d\n", fd_limit.rlim_cur, fd_limit.rlim_max); 
+}
+
+int main()
+{
   return 0;
 }
