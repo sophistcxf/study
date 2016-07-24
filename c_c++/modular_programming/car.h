432738e0389057d3f7a205b27c61226be5af7276
sophistcxf <sophistcxf@gmail.com>
2016-07-24 14:34:59 +0800

    add modular programming

---
+++ c_c++/modular_programming/car.h
@@ -0,0 +1,18 @@
+/*************************************************************************
+  > File Name: car.h
+  > Author: cxxxxf
+  > Mail: sophistcxf@gmail.com
+  > Created Time: Sun 24 Jul 2016 01:52:39 PM CST
+ ************************************************************************/
+
+#ifndef _CAR_H_
+#define _CAR_H_
+
+struct Car
+{
+  void(*run)();
+  void(*stop)();
+};
+
+#endif
+
