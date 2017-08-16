c453c61c776e1523824ed15ca4b58ba94ca7ddf8
sophistcxf <sophistcxf@gmail.com>
2017-08-16 10:30:07 +0800

    add test_iomanip.cpp

---
+++ c_c++/test_iomanip.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+  > File Name: test_iomanip.cpp
+  > Author: cxxxxf
+  > Mail: sophistcxf@gmail.com
+  > Created Time: 六  7/15 16:39:34 2017
+ ************************************************************************/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    cout << std::fixed << setprecision(3) << 13.1 << endl;
+    return 0;
+}
