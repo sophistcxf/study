a8542b5264d1fc2d7a230c113ccacc7a4f0bab76
sophistcxf <sophistcxf@gmail.com>
2017-12-22 15:46:07 +0800

    add test_heapmap.py

---
+++ seaborn/test_heapmap.py
@@ -0,0 +1,8 @@
+import numpy as np; np.random.seed(0)
+import seaborn as sns; sns.set()
+import matplotlib.pyplot as plt
+
+def test_simple():
+    uniform_data = np.random.rand(10, 12)
+    ax = sns.heatmap(uniform_data)
+    plt.show()
