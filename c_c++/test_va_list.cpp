8e6263414f246294c1d459fc5450df2c347aecc9
xiangfei.cui <xiangfei.cui@alibaba-inc.com>
2018-08-21 10:27:01 +0800

    add va_list

---
+++ c_c++/test_va_list.cpp
@@ -0,0 +1,29 @@
+/*************************************************************************
+  > File Name: test_va_list.cpp
+  > Author: xiangfei.cui
+  > Mail: xiangfei.cui@alibaba-inc.com
+  > Created Time: Mon 20 Aug 2018 05:48:41 PM CST
+ ************************************************************************/
+
+#include <iostream>
+#include <stdarg.h>
+#include <stdio.h>
+using namespace std;
+
+
+void diy_printf(const char* format, ...)
+{
+    va_list vl;
+    va_start(vl, format);
+    char buffer[256] = {0};
+    vsprintf(buffer, format, vl);
+    printf("%s", buffer);
+    va_end(vl);
+}
+
+int main(int argc, char* argv[])
+{
+    printf("my name is %s, my age is %d\n", "cxxxxf", 30); 
+    diy_printf("my name is %s, my age is %d\n", "cxxxxf", 30); 
+    return 0;
+}
