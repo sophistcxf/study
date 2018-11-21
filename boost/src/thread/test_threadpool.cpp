9c52d37a4764b8fb1cc032c61b4389eeee92c79f
xiangfei.cui <xiangfei.cui@alibaba-inc.com>
2018-11-21 18:17:47 +0800

    modi test_threadpool.cpp

--- boost/src/thread/test_threadpool.cpp
+++ boost/src/thread/test_threadpool.cpp
@@ -4,58 +4,95 @@
   > Mail: xiangfei.cui@alibaba-inc.com
   > Created Time: Tue 21 Jun 2016 02:57:47 PM CST
  ************************************************************************/
-
-#include <iostream>
-using namespace std;
-
-#include <iostream>
-#include <boost/threadpool.hpp>
+#include <boost/asio/io_service.hpp>
+#include <boost/bind.hpp>
 #include <boost/thread/thread.hpp>
 
-using namespace boost::threadpool;
 
-class Task{
+void myTask(const void* p)
+{
+    char* c = (char*)p;
+    std::cout << c << std::endl;
+}
 
-  private:
-    std::string user;
+void clearCache(const void*p)
+{
+    char* c = (char*)p;
+    std::cout << "clearCache " << c << std::endl;
+}
 
-  public:
-    Task(){}
-    ~Task(){}
+void getSocialUpdates(const void* p)
+{
+    char* c = (char*)p;
+    std::cout << c << std::endl;
+}
 
-    Task(std::string input):user(input){}
+void redelivery(const void* p)
+{
+    // gunzip
+    // base64
+    // ...
+}
 
-    void operator()(){
-      std::cout<<"Hello "<<user<<std::<span class="hiddenSpellError">endl;
-      sleep(5);
-      delete this;/*Trust me its safe */
-
-    }
+struct TwoFactor
+{
+    TwoFactor(double x, double y) : f1(x), f2(y) {}
+    double f1;
+    double f2;
 };
 
-int main(int argc, char** argv)
+void add(const void* p)
 {
-  /*Create two threads */
-  pool Thread_Pool(2);
+    TwoFactor* f = (TwoFactor*)p;
+    double sum = f->f1 + f->f2;
+    std::cout << f->f1 << "+" << f->f2 << "=" << sum << std::endl;
+}
 
-  int count;
+int main()
+{
+    /*
+     * Create an asio::io_service and a thread_group (through pool in essence)
+     */
+    boost::asio::io_service ioService;
+    boost::thread_group threadpool;
 
-  if(argc > 1)
-  {
-    for (count = 1; count < argc; count++)
-    {
 
-      Task *Thread_Object = new Task(argv[count]); /*Create an object of class Task and pass names*/
-      Thread_Pool.schedule(boost::ref(*Thread_Object));/*Schedule all threads at once */
+    /*
+     * This will start the ioService processing loop. All tasks 
+     * assigned with ioService.post() will start executing. 
+     */
+    boost::asio::io_service::work work(ioService);
 
-    }
-  }
-  else{
+    /*
+     * This will add 2 threads to the thread pool. (You could just put it in a for loop)
+     */
+     int thread_count = 10;
+     for (size_t i = 0; i < thread_count; ++i)
+        threadpool.create_thread(
+                boost::bind(&boost::asio::io_service::run, &ioService)
+                );
 
-    std::cout<<"Give some names Please  "<<std::<span class="hiddenSpellError">endl;
-  }
+    /*
+     * This will assign tasks to the thread pool. 
+     * More about boost::bind: "http://www.boost.org/doc/libs/1_54_0/libs/bind/bind.html#with_functions"
+     */
+    ioService.post(boost::bind(myTask, "Hello World!"));
+    ioService.post(boost::bind(clearCache, "./cache"));
+    ioService.post(boost::bind(getSocialUpdates, "twitter,gmail,facebook,tumblr,reddit"));
+    TwoFactor f(2.5, 10.8);
+    ioService.post(boost::bind(add, &f));
+    std::string content = "solocar_info";
 
-  std::cout<<"All Threads scheduled,now wait for their execution"<<std::<span class="hiddenSpellError">endl;
+    /*
+     * This will stop the ioService processing loop. Any tasks
+     * you add behind this point will not execute.
+     */
+    //ioService.stop();
 
-  return 0;
+    /*
+     * Will wait till all the threads in the thread pool are finished with 
+     * their assigned tasks and 'join' them. Just assume the threads inside
+     * the threadpool will be destroyed by this method.
+     */
+    threadpool.join_all();
 }
