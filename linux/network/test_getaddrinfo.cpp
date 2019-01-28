61685efeb86d2c008450559fe1fc41ef46b88d41
xiangfei.cui <xiangfei.cui@alibaba-inc.com>
2019-01-28 21:50:32 +0800

    add test_getaddrinfo.cpp

---
+++ linux/network/test_getaddrinfo.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+  > File Name: test_getaddrinfo.cpp
+  > Author: cxf 
+  > Mail: sophistcxf@gmail.com 
+  > Created Time: Mon 28 Jan 2019 09:47:15 PM CST
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+int main()
+{
+
+    int rlt = getaddrinfo(argv[1], NULL, 
+}
