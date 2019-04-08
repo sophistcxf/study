36ef5ad42b362e41a6097c3e1a9a85b904b69d8a
sophistcxf <sophistcxf@gmail.com>
2019-04-08 20:10:43 +0800

    * modi test_yield.py

--- python/test_yield/test_yield.py
+++ python/test_yield/test_yield.py
@@ -1,54 +1,60 @@
+#! -*- coding=utf-8 -*-
+
 import traceback
 
-def foo():
-  print "yield 1"
-  yield 1
-  print "yield 2"
-  yield 2
-
-p = foo()
-print "print p"
-print p
-print "print p.next() 1"
-print p.next()
-print "print p.next() 2"
-print p.next()
-try:
-  print "print p.next() 3"
-  print p.next()  # exception, StopIteration
-except Exception:
-  print traceback.print_exc()
-
-# generate object is iterable
-p = foo()
-for item in p:
-  print item
-
+def simple_test():
+    def foo():
+      yield 1
+      yield 2
+      yield 3
+    p = foo()
+    print p
+    print p.next()
+    print p.next()
+    print p.next()
+    try:
+      print p.next()  # exception, StopIteration
+    except Exception:
+      print traceback.print_exc()
+    # 遍历生成器，什么也不打
+    for i in p:
+        print i
+    p = foo()
+    # 遍历生成器，如期打印结果
+    for i in p:
+        print i
 
 def endless_generate_object():
-  number = 0
-  while True:
-    yield number
-    number += 1
+    def foo():
+        number = 0
+        while True:
+            yield number
+            number += 1
 
-p = endless_generate_object()
-for i in range(0, 100):
-  print p.next() 
+    p = foo()
+    for i in range(0, 100):
+        print p.next() 
   
 def generate_obj_send():
-  number = 0
-  while True:
-    val = yield number
-    print "hello world"
-    if val:
-      print "val: ", val
-      number = val
-    else:
-      number += 1
+    def foo():
+        number = 0
+        while True:
+            # 没有调用 send 方法时，val 是 None，当调用 send 方法时，val 是 send 方法传入的参数
+            # 即只有 send 的参数会打印出 val: val 这行日志
+            val = yield number
+            if val:
+              print "val: ", val
+              number = val
+            else:
+              number += 1
+    p = foo()
+    for i in range(5):
+        print p.next()
+    print p.send(100)
+    print p.send(200)
+    for i in range(5):
+        print p.next()
 
-p = generate_obj_send()
-print p.next()
-print p.next()
-# send 10 to generate object
-p.send(10)
-print p.next()
+#simple_test()
+#endless_generate_object()
+generate_obj_send()
