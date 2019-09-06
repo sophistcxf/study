a502fd699321ecbadaabb02c8d0412d3a7950db4
sophistcxf <sophistcxf@gmail.com>
2019-09-06 18:48:48 +0800

    * add python/overload_operator/my_series.py

---
+++ python/overload_operator/my_series.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+class Series():
+    def __init__(self):
+        self.buf = []
+    def append(self, v):
+        self.buf.append(v)
+    def __add__(self, v):
+        for i,e in enumerate(self.buf):
+            self.buf[i] += v
+
+    '''
+    支持 x in container
+    '''
+    def __contains__(self, v):
+        return v in self.buf
+
+    def to_string(self):
+        return ','.join(map(str, self.buf))
+
+s = Series()
+
+s.append(5)
+s.append(6)
+print s.to_string()
+s + 1
+print s.to_string()
+
+print 1 in s
+print 6 in s
