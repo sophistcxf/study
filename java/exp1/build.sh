7a8626e053b21c453720c978a50f1be419726135
sophistcxf <sophistcxf@gmail.com>
2020-05-28 20:35:41 +0800

    modi

--- java/exp1/build.sh
+++ java/exp1/build.sh
@@ -1,3 +1,7 @@
+# 在不同平台下，c/c++编出的库的名字是不同的，要符合平台的规范
+# 否则执行时报找不到
+# 如 mac 上是 libXXXX.dylib, Linux 上是 libXXXX.so
+
 kernel_name=`uname -s`
 if [ $kernel_name == "Darwin" ]; then
     gcc -I./ -I"/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/JavaVM.framework//Versions/A/Headers/" -fPIC -shared -o libHelloNative.dylib HelloNative.c
