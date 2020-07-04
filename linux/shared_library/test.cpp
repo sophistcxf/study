db9807e42077cf46dbf2c82a3c0abda2a2b02bcf
cxxxxf <sophistcxf@gmail.com>
2020-07-04 15:31:19 +0800

    add shared_library

---
+++ linux/shared_library/test.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+  > File Name: test.cpp
+  > Author: cxxxxf
+  > Mail: sophistcxf@gmail.com
+  > Created Time: 六  7/ 4 15:29:10 2020
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    std::cout << "hello world!" << std::endl;
+    return 0;
+}
