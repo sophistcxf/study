a7f2ae8293bd77c782ecd18dc8f2d8946ccd9b20
cxxxxf <cxxxxf@localhost.(none)>
2020-12-21 00:18:09 -0500

    modi

--- c_c++/class/inheritance/test_multi_inheritance.cpp
+++ c_c++/class/inheritance/test_multi_inheritance.cpp
@@ -1,18 +1,9 @@
+/**
+ * 有示例，不要再修改了
+ * 简单多重继承
+ */
+
 #include <iostream>
-
-class X {};
-class Y : public virtual X {};
-class Z : public virtual X {};
-class A : public Y, public Z {};
-
-void test1()
-{
-    std::cout << sizeof(X) << std::endl
-    << sizeof(Y) << std::endl
-    << sizeof(Z) << std::endl
-    << sizeof(A) << std::endl;
-}
-
 class Base1
 {
 public:
@@ -20,6 +11,8 @@ public:
     virtual ~Base1() {}
     virtual void speakClearly() {}
     virtual Base1* clone() const { return NULL; }
+    virtual void only_in_base1_virtual() {}
+    void only_in_base1_nonvirtual() {}
 protected:
     float data_base1;
 };
@@ -30,6 +23,8 @@ public:
     Base2() {}
     virtual void mumble() {}
     virtual Base2* clone() const { return NULL; }
+    virtual void only_in_base2_virtual() {}
+    void only_in_base2_nonvirtual() {}
 protected:
     float data_base2;
 };
@@ -40,22 +35,18 @@ public:
     Derived() {}
     virtual ~Derived() {}
     virtual Derived* clone() const { return NULL; }
+    virtual void only_in_derived_virtual() {}
+    void only_in_derived_nonvirtual() {}
 protected:
     float data_derived;
 };
 
-void test2()
+int main()
 {
     Derived* p = new Derived();
     Base1* p1 = p;
     Base2* p2 = p;
     std::cout << p << "\t" << p1 << "\t" << p2 << std::endl;
     std::cout << sizeof(Base1) << "\t" << (void*)((char*)p1 + sizeof(Base1)) << std::endl;
-}
-
-int main()
-{
-    //test1();
-    test2();
     return 0;
 }
--- linux_commands/objcopy/build.sh
+++ linux_commands/objcopy/build.sh
@@ -1,6 +1,8 @@
 #
 g++ -g hello_world.cpp -o hello_world
 objcopy --only-keep-debug hello_world hello_world.dbg
-objcopy --strip-debug hello_world
+objcopy --strip-debug hello_world hello_world_strip
 # 需要调试时，使用如下命令再链上符号表即可
 #objcopy --add-gnu-debuglink=foo.dbg foo
+
+objcopy -j bss hello_world hello_world hello_world_bss
