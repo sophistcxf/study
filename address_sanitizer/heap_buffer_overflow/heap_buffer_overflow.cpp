6eb13a9800d51c4ea5d705cdbce38e106c699f78
sophistcxf <sophistcxf@gmail.com>
2021-04-10 23:46:38 +0800

    modi

--- address_sanitizer/heap_buffer_overflow/heap_buffer_overflow.cpp
+++ address_sanitizer/heap_buffer_overflow/heap_buffer_overflow.cpp
@@ -1,8 +1,16 @@
 // RUN: clang -O -g -fsanitize=address %t && ./a.out
-int main(int argc, char **argv) {
+//
+#include <iostream>
+
+void test1()
+{
     int *array = new int[100];
     array[0] = 0;
-    int res = array[argc + 100];  // BOOM
+    int res = array[110];  // BOOM
     delete [] array;
-    return res;
+    std::cout << res << std::endl;
+}
+
+int main(int argc, char **argv) {
+    test1();
 }
