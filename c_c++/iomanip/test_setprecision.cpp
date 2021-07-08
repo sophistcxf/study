e31134a4ef6eb24e386281ac86fc02a6c4224ace
sophistcxf <sophistcxf@gmail.com>
2021-07-08 16:54:13 +0800

    modi setprecision

---
+++ c_c++/iomanip/test_setprecision.cpp
@@ -0,0 +1,30 @@
+/*************************************************************************
+  > File Name: test_iomanip.cpp
+  > Author: cxxxxf
+  > Mail: sophistcxf@gmail.com
+  > Created Time: 六  7/15 16:39:34 2017
+ ************************************************************************/
+
+#include <iostream>
+#include <iomanip>
+
+void test1()
+{
+    double f =3.14159;
+    std::cout << f << "\n";
+    // 3.1416
+    std::cout << std::setprecision(5) << f << '\n';
+    // 3.14159
+    std::cout << std::setprecision(9) << f << '\n';
+    std::cout << std::fixed;
+    // 3.14159
+    std::cout << std::setprecision(5) << f << '\n';
+    // 3.141590000
+    std::cout << std::setprecision(9) << f << '\n';
+}
+
+int main()
+{
+    test1();
+    return 0;
+}
