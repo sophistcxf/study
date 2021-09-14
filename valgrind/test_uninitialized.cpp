c0f1f047e8a3a4660eea706247db74ac4f7c71d1
xiangfei.cui <xiangfei.cui@alibaba-inc.com>
2021-09-14 11:29:22 +0800

    modi

--- valgrind/test_uninitialized.cpp
+++ valgrind/test_uninitialized.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
+#include <stdlib.h>
 
-struct Message {
+class Message {
+public:
+    Message() {}
     char type;
     double value;
 };
 
+/**
+ * 能检测出，但行号有偏差
+ */
 void test1() {
     int n;
     std::cout << n << std::endl;
 }
 
+/**
+ * 能检测出，但行号有偏差
+ */
 void test2() {
     Message msg;
     std::cout << msg.value << std::endl;
 }
 
+/**
+ * 能检测出
+ */
 void test3() {
     Message* msg = new Message();
     std::cout << msg->value << std::endl;
 }
 
 int main() {
-    test3();
+    test2();
 }
