920b495ac79d962f1fbbd913e2f85d08e0394f19
sophistcxf <sophistcxf@gmail.com>
2021-12-26 21:30:38 +0800

    modi

---
+++ cmake/tutorial/README.md
@@ -0,0 +1,2 @@
+官网12步教程
+https://cmake.org/cmake/help/v3.22/guide/tutorial
---
+++ cmake/tutorial/step1/exp3/tutorial.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "TutorialConfig.h"
+
+int main() {
+    std::cout << "hello world" << std::endl;
+    std::cout << "Version " << Tutorial_VERSION_MAJOR << "." << Tutorial_VERSION_MINOR << std::endl;
+    return 0;
+}
