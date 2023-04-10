d164b9fc3913a42aed788892df635382ac2f46c2
cxxxxf <sophistcxf@gmail.com>
2023-04-11 01:13:20 +0800

    modi

--- c_c++/standard_library/functional/test_placeholders.cpp
+++ c_c++/standard_library/functional/test_placeholders.cpp
@@ -7,12 +7,30 @@ int fn(int a, int b) {
     return a - b;
 }
 
-int main() {
+
+void test1() {
     auto bound_fn = std::bind (fn,100,_1);
     std::cout << bound_fn(5) << std::endl;
 
     auto fn2 = std::bind(fn, _2, _1);
     // 输出-95，而不是95
     std::cout << fn2(100, 5) << std::endl;
+}
+
+template <class T>
+void foo(T a) {
+    std::cout << std::is_placeholder<T>::value << std::endl;
+}
+
+void test2() {
+    foo(_1);    // 输出 1
+    foo(_2);    // 输出 2
+    foo(_10);   // 输出 10
+    
+    foo(10);    // 输出 0
+}
+
+int main() {
+    test2();
     return 0;
 }
