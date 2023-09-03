4d02c97ea7cd4c21768c3886de7b3d8914183d4c
cxxxxf <cuixf1988@163.com>
2023-09-03 12:07:19 +0800

    modi

---
+++ computer_architecture/program_build/exp1/sum.s
@@ -0,0 +1,39 @@
+	.file	"sum.c"
+	.text
+	.globl	_Z3sumPii
+	.type	_Z3sumPii, @function
+_Z3sumPii:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -24(%rbp)
+	movl	%esi, -28(%rbp)
+	movl	$0, -8(%rbp)
+	movl	$0, -4(%rbp)
+.L3:
+	movl	-4(%rbp), %eax
+	cmpl	-28(%rbp), %eax
+	jge	.L2
+	movl	-4(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	addl	%eax, -8(%rbp)
+	addl	$1, -4(%rbp)
+	jmp	.L3
+.L2:
+	movl	-8(%rbp), %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	_Z3sumPii, .-_Z3sumPii
+	.ident	"GCC: (GNU) 8.5.0 20210514 (Red Hat 8.5.0-4)"
+	.section	.note.GNU-stack,"",@progbits
