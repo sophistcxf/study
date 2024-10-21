15bc052155ba69f635fa99e03356c69ce13eddfb
sophistcxf <sophistcxf@gmail.com>
2024-10-21 17:47:13 +0800

    feat:modify pybind11

---
+++ pybind11/exp1/exp.py
@@ -0,0 +1,11 @@
+import example
+
+print(example.add(1, 2))
+
+print(example.add2(i=1, j=4))
+
+print(example.add3())
+
+print(example.the_answer)
+
+print(example.what)
--- pybind11/exp1/exp1.cpp
+++ pybind11/exp1/exp1.cpp
@@ -1,6 +1,16 @@
 #include <pybind11/pybind11.h>
 
+namespace py = pybind11;
+
 int add(int i, int j) {
+    return i + j;
+}
+
+int add2(int i, int j) {
+    return i + j;
+}
+
+int add3(int i = 1, int j = 2) {
     return i + j;
 }
 
@@ -8,4 +18,17 @@ PYBIND11_MODULE(example, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
 
     m.def("add", &add, "A function that adds two numbers");
+
+    m.def("add2", &add2, "A function which adds two numbers",
+      py::arg("i"), py::arg("j"));
+
+    // pybind11无法从add3中直接提取默认参数，需要显示的指定
+    m.def("add3", &add3, "A function which adds two numbers",
+      py::arg("i") = 1, py::arg("j") = 2);
+
+    // 添加属性the_answer，值为42
+    m.attr("the_answer") = 42;
+    py::object world = py::cast("World");
+    // 添加属性what，值为"World"
+    m.attr("what") = world;
 }
