eb2526c45ec8c2d7d520f807af7fa86dabd56870
sophistcxf <sophistcxf@gmail.com>
2025-07-25 10:38:19 +0800

    feat:modify simdjson

--- simdjson/exp1/main.cpp
+++ simdjson/exp1/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 #include "simdjson.h"
+
+void test1() {
+    simdjson::dom::parser parser;
+    simdjson::dom::element tweets = parser.load("twitter.json");
+    std::cout << tweets["search_metadata"]["count"] << " results." << std::endl;
+}
+
+void test2() {
+    simdjson::dom::parser parser;
+    simdjson::dom::element tweets = parser.load("twitter.json");
+    std::cout << "ID: " << tweets["statuses"].at(0)["id"] << std::endl;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    test2();
 }
