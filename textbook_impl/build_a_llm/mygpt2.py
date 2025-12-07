5e0ad5e2bc25bf2c686894d9210b6c333e167071
sophistcxf <sophistcxf@gmail.com>
2025-12-07 16:27:34 +0800

    add mygpt2.py

---
+++ textbook_impl/build_a_llm/mygpt2.py
@@ -0,0 +1,343 @@
+'''
+自实现gpt2，参考ch4,5
+'''
+
+import torch
+import torch.nn as nn
+
+import tiktoken
+def text_to_tokens_ids(text, tokenizer):
+    encoded = tokenizer.encode(text, allowed_special=set(['<|endoftext|>']))
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+    return encoded_tensor
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0)
+    return tokenizer.decode(flat.tolist())
+
+torch.manual_seed(123)
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,     # 词汇表大小
+    "context_length": 256,   # 上下文长度，最大输入词元的数量
+    "emb_dim": 768,          # 嵌入维度
+    "n_heads": 12,           # 注意力头的数量
+    "n_layers": 12,          # 层数
+    "drop_rate": 0.1,        # dropout率
+    "qkv_bias": False        # 查询-键-值偏置
+}
+
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) *
+             (x + 0.044715 * torch.pow(x, 3))
+        ))
+
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+    
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4 * cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4 * cfg["emb_dim"], cfg["emb_dim"]),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, num_heads, dropout, qkv_bias=False):
+        super().__init__()
+        assert (d_out % num_heads == 0), "d_out must be divisible by num_heads"
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer("mask", torch.tril(torch.ones(context_length, context_length), diagonal=1))
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        attn_scores = queries @ keys.transpose(2, 3)
+        mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
+
+        attn_scores.masked_fill(mask_bool, -torch.inf)
+
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+
+        context_vec = (attn_weights @ values).transpose(1, 2)
+
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+
+        context_vec = self.out_proj(context_vec)
+
+        return context_vec
+
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in = cfg["emb_dim"],
+            d_out = cfg["emb_dim"],
+            context_length = cfg["context_length"],
+            num_heads = cfg["n_heads"],
+            dropout = cfg["drop_rate"],
+            qkv_bias = cfg["qkv_bias"]
+        )
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+    
+    def forward(self, x):
+        shortcut = x
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+        return x
+
+class GPTModel(nn.Module):
+    def __init__(self, cfg:dict):
+        super().__init__();
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg)
+              for _ in range(cfg["n_layers"])]
+        )
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(
+            cfg["emb_dim"], cfg["vocab_size"], bias=False
+        )
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(
+            torch.arange(seq_len, device=in_idx.device)
+        )
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+    return idx
+
+
+def count_parameters():
+    torch.manual_seed(123)
+    model = GPTModel(GPT_CONFIG_124M)
+    total_params = sum(p.numel() for p in model.parameters())
+    print(f"Total parameters of model: {total_params}")
+    print(f"Total parameters of tok_embedding: {sum(p.numel() for p in model.tok_emb.parameters())}")
+    print(f"Total parameters of pos_embedding: {sum(p.numel() for p in model.pos_emb.parameters())}")
+    print(f"Total parameters of norm: {sum(p.numel() for p in model.final_norm.parameters())}")
+    print(f"Total parameters of out_head: {sum(p.numel() for p in model.out_head.parameters())}")
+
+    trf_params = sum(p.numel() for p in model.trf_blocks.parameters())
+    print(f"Total parameters of transformer: {trf_params}")
+
+    ff_params = 0
+    att_params = 0
+    trf_norm1_params = 0
+    trf_norm2_params = 0
+    w_query_params = 0
+    w_keys_params = 0
+    w_values_params = 0
+    out_proj_params = 0
+
+    for trf in model.trf_blocks:
+        ff_params += sum(p.numel() for p in trf.ff.parameters())
+        att_params += sum(p.numel() for p in trf.att.parameters())
+        w_query_params += sum(p.numel() for p in trf.att.W_query.parameters())
+        w_keys_params += sum(p.numel() for p in trf.att.W_key.parameters())
+        w_values_params += sum(p.numel() for p in trf.att.W_value.parameters())
+        out_proj_params += sum(p.numel() for p in trf.att.out_proj.parameters())
+        trf_norm1_params += sum(p.numel() for p in trf.norm1.parameters())
+        trf_norm2_params += sum(p.numel() for p in trf.norm2.parameters())
+
+    print(f"Total parameters of ff: {ff_params}")
+    print(f"Total parameters of att: {att_params}")
+    print(f"Total parameters of att.W_query: {w_query_params}")
+    print(f"Total parameters of att.W_keys: {w_keys_params}")
+    print(f"Total parameters of att.W_values: {w_values_params}")
+    print(f"Total parameters of att.out_proj: {out_proj_params}")
+    print(f"Total parameters of trf_norm1: {trf_norm1_params}")
+    print(f"Total parameters of trf_norm2: {trf_norm2_params}")
+    print(f"Total parameters of trf({trf_norm1_params}+{trf_norm2_params}+{att_params}+{ff_params}): {trf_norm1_params + trf_norm2_params + att_params + ff_params}")
+
+def download_gps2_params():
+    import urllib.request
+    url = (
+        "https://raw.githubusercontent.com/rasbt/"
+        "LLMs-from-scratch/main/ch05/"
+        "01_main-chapter-code/gpt_download.py"
+    )
+    filename = url.split('/')[-1]
+    urllib.request.urlretrieve(url, filename)
+
+    from gpt_download import download_and_load_gpt2
+    settings, params = download_and_load_gpt2(
+        model_size="124M", models_dir="gpt2"
+    )
+    print("Settings:", settings)
+    print("Parameter dictionary:", params.keys())
+    return settings, params
+
+settings, params = download_gps2_params()
+
+model_configs = {
+    "gpt2-small (124M)": {"emb_dim": 768, "n_layers": 12, "n_heads": 12},
+    "gpt2-medium (355M)": {"emb_dim": 1024, "n_layers": 24, "n_heads": 16},
+    "gpt2-large (774M)": {"emb_dim": 1280, "n_layers": 36, "n_heads": 20},
+    "gpt2-xl (1558M)": {"emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+}
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model_name = "gpt2-small (124M)"
+NEW_CONFIG = GPT_CONFIG_124M.copy()
+NEW_CONFIG.update(model_configs[model_name])
+
+NEW_CONFIG.update({"context_length": 1024})
+NEW_CONFIG.update({"qkv_bias": True})
+
+print(NEW_CONFIG)
+
+gpt = GPTModel(NEW_CONFIG)
+gpt.eval()
+
+def assign(left, right):
+    if left.shape != right.shape:
+        raise ValueError(f"{left.shape} != {right.shape}")
+    return torch.nn.Parameter(torch.tensor(right))
+
+def generate(model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, eos_id=None):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        if top_k is not None:
+            top_logits, _ = torch.topk(logits, k=top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(
+                logits < min_val,
+                torch.tensor(float('-inf')).to(logits.device),
+                logits
+            )
+        if temperature > 0.0:
+            logits /= temperature
+            probs = torch.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)
+        if idx_next == eos_id:
+            break
+        idx = torch.cat((idx, idx_next), dim=1)
+    return idx
+
+import numpy as np
+
+def load_weights_into_gpt(gpt : GPTModel, params):
+    gpt.pos_emb.weight = assign(gpt.pos_emb.weight, params["wpe"])
+    gpt.tok_emb.weight = assign(gpt.tok_emb.weight, params["wte"])
+
+    trf_size = len(params["blocks"])
+
+    for b in range(len(params["blocks"])):
+        q_w, k_w, v_w = np.split((params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=1)
+
+        gpt.trf_blocks[b].att.W_query.weight = assign(gpt.trf_blocks[b].att.W_query.weight, q_w.T)
+        gpt.trf_blocks[b].att.W_key.weight = assign(gpt.trf_blocks[b].att.W_key.weight, k_w.T)
+        gpt.trf_blocks[b].att.W_value.weight = assign(gpt.trf_blocks[b].att.W_value.weight, v_w.T)
+
+        q_b, k_b, v_b = np.split((params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
+        gpt.trf_blocks[b].att.W_query.bias = assign(gpt.trf_blocks[b].att.W_query.bias, q_b)
+        gpt.trf_blocks[b].att.W_key.bias = assign(gpt.trf_blocks[b].att.W_key.bias, k_b)
+        gpt.trf_blocks[b].att.W_value.bias = assign(gpt.trf_blocks[b].att.W_value.bias, v_b)
+ 
+        gpt.trf_blocks[b].att.out_proj.weight = assign(gpt.trf_blocks[b].att.out_proj.weight, params["blocks"][b]["attn"]["c_proj"]["w"].T)
+        gpt.trf_blocks[b].att.out_proj.bias = assign(gpt.trf_blocks[b].att.out_proj.bias, params["blocks"][b]["attn"]["c_proj"]["b"])
+
+        gpt.trf_blocks[b].ff.layers[0].weight = assign(gpt.trf_blocks[b].ff.layers[0].weight, params["blocks"][b]["mlp"]["c_fc"]["w"].T)
+        gpt.trf_blocks[b].ff.layers[0].bias = assign(gpt.trf_blocks[b].ff.layers[0].bias, params["blocks"][b]["mlp"]["c_fc"]["b"])
+        gpt.trf_blocks[b].ff.layers[2].weight = assign(gpt.trf_blocks[b].ff.layers[2].weight, params["blocks"][b]["mlp"]["c_proj"]["w"].T)
+        gpt.trf_blocks[b].ff.layers[2].bias = assign(gpt.trf_blocks[b].ff.layers[2].bias, params["blocks"][b]["mlp"]["c_proj"]["b"])
+
+        gpt.trf_blocks[b].norm1.scale = assign(gpt.trf_blocks[b].norm1.scale, params["blocks"][b]["ln_1"]["g"])
+        gpt.trf_blocks[b].norm1.shift = assign(gpt.trf_blocks[b].norm1.shift, params["blocks"][b]["ln_1"]["b"])
+        gpt.trf_blocks[b].norm2.scale = assign(gpt.trf_blocks[b].norm2.scale, params["blocks"][b]["ln_2"]["g"])
+        gpt.trf_blocks[b].norm2.shift = assign(gpt.trf_blocks[b].norm2.shift, params["blocks"][b]["ln_2"]["b"])
+
+    gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
+    gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
+    gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
+
+load_weights_into_gpt(gpt, params)
+gpt.to(device)
+
+tokenizer = tiktoken.get_encoding("gpt2")
+torch.manual_seed(123)
+token_ids = generate(
+    model=gpt,
+    idx=text_to_tokens_ids("Every effort moves you", tokenizer).to(device),
+    max_new_tokens=25,
+    context_size=NEW_CONFIG["context_length"],
+    top_k=50,
+    temperature=1.5
+)
+print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
