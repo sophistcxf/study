08a0a53c9d7bd8915ed2cd4f6479934013902c8d
sophistcxf <sophistcxf@gmail.com>
2025-12-07 16:51:57 +0800

    modify mygpt2.py

--- textbook_impl/build_a_llm/mygpt2.py
+++ textbook_impl/build_a_llm/mygpt2.py
@@ -74,7 +74,7 @@ class MultiHeadAttention(nn.Module):
         self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
         self.out_proj = nn.Linear(d_out, d_out)
         self.dropout = nn.Dropout(dropout)
-        self.register_buffer("mask", torch.tril(torch.ones(context_length, context_length), diagonal=1))
+        self.register_buffer("mask", torch.triu(torch.ones(context_length, context_length), diagonal=1))
 
     def forward(self, x):
         b, num_tokens, d_in = x.shape
@@ -93,7 +93,7 @@ class MultiHeadAttention(nn.Module):
         attn_scores = queries @ keys.transpose(2, 3)
         mask_bool = self.mask.bool()[:num_tokens, :num_tokens]
 
-        attn_scores.masked_fill(mask_bool, -torch.inf)
+        attn_scores.masked_fill_(mask_bool, -torch.inf)
 
         attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
         attn_weights = self.dropout(attn_weights)
@@ -324,7 +324,10 @@ def load_weights_into_gpt(gpt : GPTModel, params):
 
     gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
     gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
-    gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
+
+    # out_head 和 tok_embedding 共用参数
+    #gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
+    gpt.out_head.weight = gpt.tok_emb.weight
 
 load_weights_into_gpt(gpt, params)
 gpt.to(device)
